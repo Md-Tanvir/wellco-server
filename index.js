878eeb85a0c7aa7fc8b8fb19cb0238cf6b4594ac
Md-Tanvir <contact.mdtanvirahmed@gmail.com>
2022-01-12 02:01:45 +0600

    added way to save user data

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b0w5j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -18,36 +21,49 @@ app.get("/", (req, res) => {
   res.send("Wellco Server Is Running");
 });
 
-
 client.connect((err) => {
-    const coursesCollection = client.db("wellco").collection("courses");
-    const reviewsCollection = client.db("wellco").collection("reviews");
-    // const ordersCollection = client.db("wellco").collection("orders");
-    // const usersCollection = client.db("wellco").collection("users");
-    const blogsCollection = client.db("wellco").collection("blogs");
+  const coursesCollection = client.db("wellco").collection("courses");
+  const reviewsCollection = client.db("wellco").collection("reviews");
+  // const ordersCollection = client.db("wellco").collection("orders");
+  const usersCollection = client.db("wellco").collection("users");
+  const blogsCollection = client.db("wellco").collection("blogs");
 
+  // GET ALL COURSES
+  app.get("/courses", async (req, res) => {
+    const result = await coursesCollection.find({}).toArray();
+    res.send(result);
+  });
 
-    // GET ALL COURSES
-    app.get("/courses", async (req, res) => {
-      const result = await coursesCollection.find({}).toArray();
-      res.send(result);
-    });
+  // GET ALL REVIEWS
+  app.get("/reviews", async (req, res) => {
+    const result = await reviewsCollection.find({}).toArray();
+    res.send(result);
+  });
 
-    // GET ALL REVIEWS
-    app.get("/reviews", async (req, res) => {
-        const result = await reviewsCollection.find({}).toArray();
-        res.send(result);
-    });
+  // GET ALL BLOGS
+  app.get("/blogs", async (req, res) => {
+    const result = await blogsCollection.find({}).toArray();
+    res.send(result);
+  });
 
-    // GET ALL BLOGS
-    app.get("/blogs", async (req, res) => {
-        const result = await blogsCollection.find({}).toArray();
-        res.send(result);
-    });
+  // MAKING USER COLLECTION
+  app.put("/users", async (req, res) => {
+    const user = req.body;
+    const filter = { email: user.email };
+    const options = { upsert: true };
+    const updateDoc = { $set: user };
+    const result = await usersCollection.updateOne(filter, updateDoc, options);
+    res.send(result);
+  });
 
+  // MAKING USER DATA
+  app.post("/users", async (req, res) => {
+    const user = req.body;
+    const result = await usersCollection.insertOne(user);
+    res.send(result);
+  });
 });
 
-
 app.listen(port, () => {
-    console.log(`Server Is Running On Port:`, port);
-  });
+  console.log(`Server Is Running On Port:`, port);
+});
